b27166c780e18d461d01a4b60ebd1b120bc72939
edgar9316320 <riveroedgar.jm@gmail.com>
2020-04-02 21:54:41 -0500

    agregando seeds2

--- database/seeds/TransferSeeder.php
+++ database/seeds/TransferSeeder.php
@@ -11,6 +11,20 @@ class TransferSeeder extends Seeder
      */
     public function run()
     {
-        //
+
+        DB::table('transfers')->insert([[
+            'description' => 'Salary',
+            'amount' => '4800',
+            'wallet_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ], [
+            'description' => 'Rent',
+            'amount' => '-1200',
+            'wallet_id' => 1,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]]);
     }
+
 }
--- database/seeds/WalletSeeder.php
+++ database/seeds/WalletSeeder.php
@@ -11,6 +11,10 @@ class WalletSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('wallets')->insert([
+            'money' => 3600.00,
+            'created_at' => date("Y-m-d H:i:s"),
+            'updated_at' => date("Y-m-d H:i:s")
+        ]);
     }
 }
